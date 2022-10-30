f81c60cc4a40ad9276ccb534f94c57ea400ec7fd
asghjaghga <86305747+asghjaghga@users.noreply.github.com>
2022-10-30 21:50:27 +0800

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 <form action="welcome.php" method="post">
-Name: <input type="text" name="fname" placeholder="请输入用户名" requierd>
+Name: <input type="text" name="fname" placeholder="请输入用户名" requierd><br>
 Age : <input type="text" name="age" placeholder="年龄" required>
 <input type="submit">
 </form>

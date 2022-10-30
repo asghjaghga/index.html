702549d3f1912473206c887cb5c870c674d0fb46
asghjaghga <86305747+asghjaghga@users.noreply.github.com>
2022-10-30 21:57:20 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 
 <html>
 <body>
+<img src="https://atts.w3cschool.cn/rabbitwhite300.jpg" alt="白色的兔子">
 <form action="welcome.php" method="post">
 Name: <input type="text" name="fname" placeholder="请输入用户名" requierd><br>
 Age : <input type="text" name="age" placeholder="年龄" required>

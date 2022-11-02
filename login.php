4d1ead686cb6f689e9285f24f141673dc36e21a0
asghjaghga <86305747+asghjaghga@users.noreply.github.com>
2022-11-02 15:54:23 +0800

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 <img src="https://atts.w3cschool.cn/rabbitwhite300.jpg" alt="白色的兔子">
-<form action="welcome.php" method="post">
+<form action="#" method="post">
 用户名: <br>
 <input type="text" name="fname" placeholder="请输入用户名" requierd><br>
 密码 : <br>

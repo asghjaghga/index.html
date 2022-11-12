4448e3e002096e8a5e4ccbc8541a0698d278a34c
asghjaghga <86305747+asghjaghga@users.noreply.github.com>
2022-11-12 19:40:20 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,17 @@
-
 <html>
+<div class="warning">你好😊</div>
+<style>
+.warning {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 0.5rem;
+  background: linear-gradient(#fff94d, #ddd610);
+  opacity: 0.95;
+  text-align: center;
+  z-index: 1000; }
+</style>
 <body>
 <img src="https://atts.w3cschool.cn/rabbitwhite300.jpg" alt="白色的兔子">
 <form action="#" method="post">
